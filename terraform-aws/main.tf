60a7f1b777ffe10c773f813604bf2231551f34d3
BHabibMeetKai <bade.habib@meetkai.com>
2026-06-03 16:47:36 -0700

    feat: added additional settings to maintf

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -2,31 +2,102 @@ provider "aws" {
   region = "us-west-2"
 }
 
-data "aws_s3_bucket" "test" {
+locals {
+  s3_origin_id = "X"
+  my_domain    = "X.com"
+}
+
+data "aws_acm_certificate" "my_domain" {
+  region   = "us-east-1"
+  domain   = "*.${local.my_domain}"
+  statuses = ["ISSUED"]
+}
+
+resource "aws_s3_bucket" "test" {
   bucket = "test-bucket"
+
+  tags = {
+    Name = "Test bucket"
+  }
+}
+
+resource "aws_cloudfront_origin_access_control" "default" {
+  name                              = "default-oac"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
 }
 
 # Ref: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudfront_distribution
 resource "aws_cloudfront_distribution" "test" {
   origin {
-    domain_name = data.aws_s3_bucket.test.bucket_domain_name
-    origin_id   = "s3-test-bucket"
+    domain_name              = data.aws_s3_bucket.test.bucket_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.default.id
+    origin_id                = "s3-test-bucket"
   }
 
-  viewer_certificate {}
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
 
-  restrictions {
-    geo_restriction {
-      restriction_type = ""
+  default_cache_behavior {
+    allowed_methods        = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = local.s3_origin_id
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
     }
   }
 
-  default_cache_behavior {
-    allowed_methods = [ "GET" ]
-    cached_methods = [ "GET" ]
-    target_origin_id = ""
-    viewer_protocol_policy = ""
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US"]
+    }
   }
 
-  enabled = true
+  viewer_certificate {
+    acm_certificate_arn = data.aws_acm_certificate.my_domain.arn
+    ssl_support_method  = "sni-only"
+  }
+}
+
+# See https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/private-content-restricting-access-to-s3.html
+data "aws_iam_policy_document" "origin_bucket_policy" {
+  statement {
+    sid    = "AllowCloudFrontServicePrincipalReadWrite"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.b.arn}/*",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.s3_distribution.arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "test" {
+  bucket = aws_s3_bucket.test.bucket
+  policy = data.aws_iam_policy_document.origin_bucket_policy.json
 }
