ecd93f0106062d169cac5eeb0754f5c3b371200f
Bade Habib <bjhabib93@gmail.com>
2026-06-10 16:10:16 -0700

    Configured terraform state and plan for AWS S3, configured github actions for S3 deployment

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -1,103 +1,101 @@
-provider "aws" {
-  region = "us-west-2"
-}
+provider "aws" {}
 
-locals {
-  s3_origin_id = "X"
-  my_domain    = "X.com"
-}
+# locals {
+#   s3_origin_id = "X"
+#   my_domain    = "promise-protocol.com"
+# }
 
-data "aws_acm_certificate" "my_domain" {
-  region   = "us-east-1"
-  domain   = "*.${local.my_domain}"
-  statuses = ["ISSUED"]
-}
+# data "aws_acm_certificate" "my_domain" {
+#   region   = "us-east-1"
+#   domain   = "*.${local.my_domain}"
+#   statuses = ["ISSUED"]
+# }
 
-resource "aws_s3_bucket" "test" {
-  bucket = "test-bucket"
+resource "aws_s3_bucket" "example" {
+  bucket = "my-test-bucket-350"
 
   tags = {
     Name = "Test bucket"
   }
 }
 
-resource "aws_cloudfront_origin_access_control" "default" {
-  name                              = "default-oac"
-  origin_access_control_origin_type = "s3"
-  signing_behavior                  = "always"
-  signing_protocol                  = "sigv4"
-}
+# resource "aws_cloudfront_origin_access_control" "default" {
+#   name                              = "default-oac"
+#   origin_access_control_origin_type = "s3"
+#   signing_behavior                  = "always"
+#   signing_protocol                  = "sigv4"
+# }
 
 # Ref: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudfront_distribution
-resource "aws_cloudfront_distribution" "test" {
-  origin {
-    domain_name              = data.aws_s3_bucket.test.bucket_domain_name
-    origin_access_control_id = aws_cloudfront_origin_access_control.default.id
-    origin_id                = "s3-test-bucket"
-  }
+# resource "aws_cloudfront_distribution" "s3_distribution" {
+#   origin {
+#     domain_name              = data.aws_s3_bucket.test.bucket_domain_name
+#     origin_access_control_id = aws_cloudfront_origin_access_control.default.id
+#     origin_id                = "s3-test-bucket"
+#   }
 
-  enabled             = true
-  is_ipv6_enabled     = true
-  default_root_object = "index.html"
+#   enabled             = true
+#   is_ipv6_enabled     = true
+#   default_root_object = "index.html"
 
-  default_cache_behavior {
-    allowed_methods        = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods         = ["GET", "HEAD"]
-    target_origin_id       = local.s3_origin_id
-    viewer_protocol_policy = "allow-all"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-    forwarded_values {
-      query_string = false
-      cookies {
-        forward = "none"
-      }
-    }
-  }
+#   default_cache_behavior {
+#     allowed_methods        = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+#     cached_methods         = ["GET", "HEAD"]
+#     target_origin_id       = local.s3_origin_id
+#     viewer_protocol_policy = "allow-all"
+#     min_ttl                = 0
+#     default_ttl            = 3600
+#     max_ttl                = 86400
+#     forwarded_values {
+#       query_string = false
+#       cookies {
+#         forward = "none"
+#       }
+#     }
+#   }
 
-  restrictions {
-    geo_restriction {
-      restriction_type = "whitelist"
-      locations        = ["US"]
-    }
-  }
+#   restrictions {
+#     geo_restriction {
+#       restriction_type = "whitelist"
+#       locations        = ["US"]
+#     }
+#   }
 
-  viewer_certificate {
-    acm_certificate_arn = data.aws_acm_certificate.my_domain.arn
-    ssl_support_method  = "sni-only"
-  }
-}
+#   viewer_certificate {
+#     acm_certificate_arn = data.aws_acm_certificate.my_domain.arn
+#     ssl_support_method  = "sni-only"
+#   }
+# }
 
 # See https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/private-content-restricting-access-to-s3.html
-data "aws_iam_policy_document" "origin_bucket_policy" {
-  statement {
-    sid    = "AllowCloudFrontServicePrincipalReadWrite"
-    effect = "Allow"
+# data "aws_iam_policy_document" "origin_bucket_policy" {
+#   statement {
+#     sid    = "AllowCloudFrontServicePrincipalReadWrite"
+#     effect = "Allow"
 
-    principals {
-      type        = "Service"
-      identifiers = ["cloudfront.amazonaws.com"]
-    }
+#     principals {
+#       type        = "Service"
+#       identifiers = ["cloudfront.amazonaws.com"]
+#     }
 
-    actions = [
-      "s3:GetObject",
-      "s3:PutObject",
-    ]
+#     actions = [
+#       "s3:GetObject",
+#       "s3:PutObject",
+#     ]
 
-    resources = [
-      "${aws_s3_bucket.b.arn}/*",
-    ]
+#     resources = [
+#       "${aws_s3_bucket.test.arn}/*",
+#     ]
 
-    condition {
-      test     = "StringEquals"
-      variable = "AWS:SourceArn"
-      values   = [aws_cloudfront_distribution.s3_distribution.arn]
-    }
-  }
-}
+#     condition {
+#       test     = "StringEquals"
+#       variable = "AWS:SourceArn"
+#       values   = [aws_cloudfront_distribution.s3_distribution.arn]
+#     }
+#   }
+# }
 
-resource "aws_s3_bucket_policy" "test" {
-  bucket = aws_s3_bucket.test.bucket
-  policy = data.aws_iam_policy_document.origin_bucket_policy.json
-}
+# resource "aws_s3_bucket_policy" "test" {
+#   bucket = aws_s3_bucket.test.bucket
+#   policy = data.aws_iam_policy_document.origin_bucket_policy.json
+# }
